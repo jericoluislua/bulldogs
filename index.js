10d296bff57cee6f93a895150193199c41466e04
jericoluislua <jericoluislua@yahoo.com.ph>
2019-10-31 00:41:52 +0100

    updated

--- index.js
+++ index.js
@@ -4,19 +4,38 @@ const mysql = require('mysql');
 
 const app = express();
 
-const SELECT_ALL_BPLAYERS_QUERY = 'SELECT * FROM players';
+const SELECT_ALL_BPLAYERS_QUERY = 'SELECT * FROM player';
 
 const connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: 'Jerico2705',
-    database:
-})
+    password: 'Jerico.aomine5',
+    database: 'bulldogs'
+});
+
+connection.connect(err => {
+    if(err) {
+        return err;
+    }
+});
 
 app.use(cors());
-//request n respond
+
 app.get('/', (req, res) => {
-    res.send('Go to /')
+    res.send('go to /player to see players')
+});
+//request n respond
+app.get('/player', (req, res) => {
+    connection.query(SELECT_ALL_BPLAYERS_QUERY, (err, results) => {
+        if(err) {
+            return res.send(err)
+        }
+        else {
+            return res.json({
+                data: results
+            })
+        }
+    });
 });
 
 app.listen(4000, () => {
