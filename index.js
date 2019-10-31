08bb2067ee6044cc025a0da9632c42c7d041cb54
Administrator adm-virtedu <adm-virtedu@iet-gibb.ch>
2019-10-31 11:18:07 +0100

    works insert

--- index.js
+++ index.js
@@ -1,19 +1,22 @@
 const express = require('express');
 const cors = require('cors');
 const mysql = require('mysql');
+const bodyparser = require('body-parser');
 
 const app = express();
 
 const SELECT_ALL_BPLAYERS_QUERY = 'SELECT * FROM player';
 
-const connection = mysql.createConnection({
+const db_config = {
     host: 'localhost',
     user: 'root',
     password: 'Jerico.aomine5',
     database: 'bulldogs'
-});
+};
 
-connection.connect(err => {
+const connection = mysql.createConnection(db_config);
+
+connection.query(err => {
     if(err) {
         return err;
     }
@@ -24,16 +27,31 @@ app.use(cors());
 app.get('/', (req, res) => {
     res.send('go to /player to see players')
 });
+
+app.get('/player/add',(req, res) =>{
+   const { username, password, firstName, lastName, jerseyNumber, height, weight, isFormer, isAdmin } = req.query;
+   const INSERT_PRODUCTS_QUERY =
+       `INSERT INTO player(username, password, firstName, lastName, jerseyNumber)` +
+       `VALUES('${username}', '${password}', '${firstName}', '${lastName}', '${jerseyNumber}')`;
+   connection.query(INSERT_PRODUCTS_QUERY, (err, results) => {
+       if(err) {
+           return res.send(err);
+       }
+       else {
+           return res.send('successfully added product')
+       }
+   });
+});
 //request n respond
 app.get('/player', (req, res) => {
     connection.query(SELECT_ALL_BPLAYERS_QUERY, (err, results) => {
         if(err) {
-            return res.send(err)
+            return res.send(err);
         }
         else {
             return res.json({
-                data: results
-            })
+                data:results
+            });
         }
     });
 });
