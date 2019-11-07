4aafee6195e3a5bbf9661b4dfc918e552dc5a47c
Administrator adm-virtedu <adm-virtedu@iet-gibb.ch>
2019-11-07 08:29:37 +0100

    removed header directory and its files inside

--- react-client/src/components/Navbar/Navbar.js
+++ react-client/src/components/Navbar/Navbar.js
@@ -5,7 +5,7 @@ import React from 'react';
 function Navbar() {
     return (
         <nav className="navbar navbar-expand-lg navbar-light">
-            <a className="navbar-brand text-uppercase" href="#">Bulldogs</a>
+            <a className="navbar-brand text-uppercase center" href="#">Bulldogs</a>
             <button className="navbar-toggler" type="button" data-toggle="collapse"
                     data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
                     aria-label="Toggle navigation">
