dc82c56b35ef01c9d3fe9b9e1db103b1c54e9f42
jericoluislua <jericoluislua@yahoo.com.ph>
2019-11-09 18:08:02 +0100

    added the logo font

--- react-client/src/components/Navbar/Navbar.js
+++ react-client/src/components/Navbar/Navbar.js
@@ -1,11 +1,11 @@
 //rcjc for automatic class creation
 import './Navbar.css';
 import React from 'react';
-
+import bulldogsFont from '../../fonts/aspire-smallcaps/AspireSC-Regular.ttf';
 function Navbar() {
     return (
         <nav className="navbar navbar-expand-lg navbar-light">
-            <a className="navbar-brand text-uppercase center" href="#">Bulldogs</a>
+            <a className="navbar-brand text-uppercase center" id="nBrand" href="#">Bulldogs</a>
             <button className="navbar-toggler" type="button" data-toggle="collapse"
                     data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
                     aria-label="Toggle navigation">
